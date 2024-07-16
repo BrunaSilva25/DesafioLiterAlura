06db8f938cf7a6e33c5a32a1dd8d5c115d138c79
BrunaSilva25 <78625315+BrunaSilva25@users.noreply.github.com>
2024-07-16 01:38:19 -0300

    first commit

---
+++ Challenge-LiterAlura-main/src/main/java/br/com/bruna/literalura/LiteraluraApplication.java
@@ -0,0 +1,22 @@
+package br.com.bruna.literalura;
+
+import br.com.bruna.literalura.menu.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+    @Autowired
+    private Menu app;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        app.run();
+    }
+}
